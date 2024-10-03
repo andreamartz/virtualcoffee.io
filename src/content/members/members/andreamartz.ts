6c1374b0b63a44fa7597b98bf97064ef179abd52
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2024-10-03 14:01:45 -0500

    Update my VC member info

--- src/content/members/members/andreamartz.ts
+++ src/content/members/members/andreamartz.ts
@@ -1,6 +1,7 @@
 import type { MemberObject } from '../types';
-// import { profileMasks } from '../flare';
+import { profileMasks } from '../flare';
 
+// Change _EXAMPLE to `yourGitHubUserName` and add your info below
 export const andreamartz: MemberObject = {
 	// GitHub username (required)
 	github: 'andreamartz',
@@ -11,10 +12,11 @@ export const andreamartz: MemberObject = {
 	name: 'Andrea Martz',
 	//
 	// Main URL - If not defined here, it will default to the website displayed on your GitHub profile. If that's not defined, then a link to your GitHub profile will be displayed.
-	// mainUrl: 'https://andreamartz.dev/',
-	//
+  mainUrl: 'https://andreamartz.dev/',
+  //
 	// Bio - Accepts [markdown](https://spec.commonmark.org/0.30/). Please keep your bio to a reasonable length. Refer to our [members page](https://virtualcoffee.io/members/) for examples.
-	bio: `Full-stack dev | Avid learner | Passionate about education and equity & using tech to make it happen`,
+	//	bio: `Full-stack dev | Avid learner | Passionate about education and equity & using tech to make it happen`,
+	bio: `Fullstack software engineer | NextJS | ReactJS | Node | ExpressJS | Passionate about education, equity, and accessibility`,
 	//
 	// flare - If you want to add a flare to your profile, you can do so here.
 	// Uncomment the `import { profileMasks } from '../flare';` line at the top of this file to use the profileMasks.
@@ -25,8 +27,7 @@ export const andreamartz: MemberObject = {
 	// profileMasks.circle, profileMasks.rabbet, profileMasks.star
 	// you can also use a custom string. the profileMask values are [css clipPath values](https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path)
 	flare: {
-		// example:
-		// profileMask: profileMasks.triangle,
+		profileMask: profileMasks.circle,
 	},
 	//
 	// Links - You can add one of each type, except website - you can add as many `website` accounts as you wish.
@@ -40,11 +41,20 @@ export const andreamartz: MemberObject = {
 		// { type: 'polywork', username: 'yourUserName' },
 		// { type: 'medium', username: 'yourUserName' },
 		// { type: 'hashnode', username: 'yourUserName' },
+		// { type: 'mastodon', url: 'https://mastodon.server/@username' },
+		// { type: 'website', url: 'https://virtualcoffee.io', title: 'Title of link' },
 		{
 			type: 'website',
 			url: 'https://andreamartz.dev/',
 			title: "Andrea's Portfolio",
 		},
 	],
-	badges: [],
+	badges: ['Hacktoberfest2024'],
+	// Add your location to our member map at https://virtualcoffee.io/members (optional)
+	// Feel free to be as specific or vague as you're comfortable with.
+	location: {
+		latitude: 44.97997,
+		longitude: -93.26384,
+		title: 'Minneapolis, MN', // optional
+	},
 };
